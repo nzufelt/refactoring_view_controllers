4d00b898bc0c5d32c00b26c862aafb097ab9b9cd
Nicholas Zufelt <nicholas.zufelt@gmail.com>
2020-01-31 13:34:56 -0500

    Initial Commit

---
+++ RefactoringViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RefactoringViewControllers
+//
+//  Created by Nicholas Zufelt on 1/31/20.
+//  Copyright © 2020 Nicholas Zufelt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
